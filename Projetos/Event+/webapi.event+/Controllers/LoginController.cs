975bde3c72174a4b3933912e2480a1feab3e5317
Artur Fiorentino <afiorentino1415@gmail.com>
2023-09-19 18:49:37 -0300

    Criação de Login e implementação da JWT

--- Projetos/Event+/webapi.event+/Controllers/LoginController.cs
+++ Projetos/Event+/webapi.event+/Controllers/LoginController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using webapi.event_.Domains;
 using webapi.event_.Interfaces;
 using webapi.event_.Repositories;
@@ -24,8 +27,61 @@ namespace webapi.event_.Controllers
         {
             try
             {
-                Usuario usuarioBuscado = _usuarioRepository.(usuario.Email, usuario.Senha);
+                Usuario usuarioBuscado = _usuarioRepository.BuscarPorEmailESenha(usuario.Email, usuario.Senha);
+                if (usuarioBuscado == null)
+                {
+                    return NotFound("Email ou senha inválido!");
+                }
+                //Caso encontre o usuário, prossegue para criação do token
 
+                //1º - definir as informações(Claims) que serão fornecidas no toen(PAYLOAD)
+                var claims = new[]
+                {
+                    //formado da claim
+                    new Claim(JwtRegisteredClaimNames.Jti,usuarioBuscado.IdUsuario.ToString()),
+                    new Claim(JwtRegisteredClaimNames.Email,usuarioBuscado.Email),
+                    new Claim(ClaimTypes.Role,usuarioBuscado.TipoUsuario.Titulo),
+
+                    //Existe a possibilidade de criar um claim personalizada
+                    //new Claim("Claim personalizada","Valor da Claim personalizada")
+                };
+
+                //2º - Definir a chave de acesso ao token
+                var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("event-chave-autenticacao-webapi-dev"));
+
+                //3º - Definir as credenciais do token(HEADER)
+                var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+                //4º - Gerar o token
+                var token = new JwtSecurityToken
+                (
+                 //emissor do token 
+                 issuer: "webapi.event+",
+
+                 //destinatário do token
+                 audience: "webapi.event+",
+
+                 //Dados definidos nas claims(informações)
+                 claims: claims,
+
+                 //tempo de expiração do token 
+                 expires: DateTime.Now.AddMinutes(5),
+
+                 //credenciais do tolen
+                 signingCredentials: creds
+                );
+
+                //5º retornar o token criado
+                return Ok(new
+                {
+                    token = new JwtSecurityTokenHandler().WriteToken(token)
+                });
+
+
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
             }
         }
     }
--- Projetos/Event+/webapi.event+/Utils/Criptografia.cs
+++ Projetos/Event+/webapi.event+/Utils/Criptografia.cs
@@ -2,14 +2,24 @@
 {
     public static class Criptografia
     {
+        /// <summary>
+        /// Gera uma hash a partir de uma senha
+        /// </summary>
+        /// <param name="senha">Senha que receberá a hash</param>
+        /// <returns>Senha criptografada</returns>
         public static string GerarHash(string senha)
         {
             return BCrypt.Net.BCrypt.HashPassword(senha);
         }
-
-        public static bool CompararHash(string senhaForm, string senhaBanco)
+        /// <summary>
+        /// Verifica se a hash da senha informada é igual a hash salva no banco
+        /// </summary>
+        /// <param name="senhaForm">Senha informada pelo usuário</param>
+        /// <param name="senhaHash">Senha cadastrada no banco</param>
+        /// <returns>True or False (senha verdadeira ?)</returns>
+        public static bool CompararHash(string senhaForm, string senhaHash)
         {
-            return BCrypt.Net.BCrypt.Verify(senhaBanco, senhaForm);
+            return BCrypt.Net.BCrypt.Verify(senhaForm, senhaHash);
         }
     }
 }
