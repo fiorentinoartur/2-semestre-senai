cecd478967fef06b9a2b4717bdc1aba4c7c121af
Artur Fiorentino <49957976885@info.local>
2023-10-25 10:50:56 -0300

    Introdução a metodologia BEM

---
+++ dotnet8/Program.cs
@@ -0,0 +1,17 @@
+﻿int numero = int.Parse(Console.ReadLine()!);
+
+string diaDaSemana = numero switch 
+{
+    1 => "Domingo",
+    2 => "Segunda",
+    3 => "Terça",
+    _ => "Dia inválido"
+};
+Console.WriteLine(diaDaSemana);
+
+
+
+
+
+
+
